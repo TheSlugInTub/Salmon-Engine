746a5510b2810fc900ba15eb33e083bd484a97a5
unknown <aseedadad@gmail.com>
2024-11-26 13:59:00 +0500

    Moved_syslocked_to_different_branch

---
+++ src/extra_systems.cpp
@@ -0,0 +1,48 @@
+#include <ecs.h>
+#include <engine.h>
+#include <input.h>
+#include <components.h>
+
+void CameraMoveSys()
+{
+    if (glfwGetKey(engineState.window->window, GLFW_KEY_W) == GLFW_PRESS)
+        engineState.camera->ProcessKeyboard(CameraMovement::FORWARD, engineState.deltaTime);
+    if (glfwGetKey(engineState.window->window, GLFW_KEY_S) == GLFW_PRESS)
+        engineState.camera->ProcessKeyboard(CameraMovement::BACKWARD, engineState.deltaTime);
+    if (glfwGetKey(engineState.window->window, GLFW_KEY_A) == GLFW_PRESS)
+        engineState.camera->ProcessKeyboard(CameraMovement::LEFT, engineState.deltaTime);
+    if (glfwGetKey(engineState.window->window, GLFW_KEY_D) == GLFW_PRESS)
+        engineState.camera->ProcessKeyboard(CameraMovement::RIGHT, engineState.deltaTime);
+}
+
+void CameraLookSys()
+{
+    // Variables to store the last mouse position
+    static bool firstMouse = true;
+    static float lastX = 0.0f, lastY = 0.0f;
+
+    // Get the current mouse position
+    double xpos, ypos;
+    glfwGetCursorPos(engineState.window->window, &xpos, &ypos);
+
+    if (firstMouse)
+    {
+        lastX = static_cast<float>(xpos);
+        lastY = static_cast<float>(ypos);
+        firstMouse = false;
+    }
+
+    // Calculate the offset from the last position
+    float xoffset = static_cast<float>(xpos) - lastX;
+    float yoffset = lastY - static_cast<float>(ypos); // Reversed since y-coordinates go from bottom to top
+
+    // Update the last position
+    lastX = static_cast<float>(xpos);
+    lastY = static_cast<float>(ypos);
+
+    // Process the mouse movement with the calculated offset
+    engineState.camera->ProcessMouseMovement(xoffset, yoffset);
+}
+
+REGISTER_SYSTEM(CameraMoveSys);
+REGISTER_SYSTEM(CameraLookSys);
---
+++ src/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <salmon.h>
+#include <string>
+
+// settings
+const unsigned int SCR_WIDTH = 1920;
+const unsigned int SCR_HEIGHT = 1080;
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+float physTimer = 0.3f;
+
+int main()
+{
+    Window window("Prism", SCR_WIDTH, SCR_HEIGHT, false);
+    glfwSetInputMode(window.window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSwapInterval(1);
+
+    StartPhysics();
+
+    Model boxModel("res/models/Box.obj");
+    unsigned int groundTex = Utils::LoadTexture("res/textures/background.png");
+    unsigned int slugariusTex = Utils::LoadTexture("res/textures/Slugarius.png");
+
+    Scene scene;
+
+    EntityID box = scene.AddEntity();
+    scene.AssignParam<Transform>(box, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f),
+                                 glm::vec3(1.0f, 1.0f, 1.0f));
+    scene.AssignParam<MeshRenderer>(box, boxModel, glm::vec4(1.0f, 0.0f, 1.0f, 1.0f), slugariusTex);
+    scene.AssignParam<RigidBody3D>(box, ColliderType::Box, BodyState::Dynamic, glm::vec3(1.0f, 1.0f, 1.0f));
+
+    EntityID ground = scene.AddEntity();
+    scene.AssignParam<Transform>(ground, glm::vec3(0.0f, -3.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f),
+                                 glm::vec3(10.0f, 1.0f, 10.0f));
+    scene.AssignParam<MeshRenderer>(ground, boxModel, glm::vec4(1.0f, 0.0f, 1.0f, 1.0f), groundTex);
+    scene.AssignParam<RigidBody3D>(ground, ColliderType::Box, BodyState::Static, glm::vec3(10.0f, 1.0f, 10.0f));
+
+    EntityID light2 = scene.AddEntity();
+    scene.AssignParam<Light>(light2, glm::vec3(0.0f, 10.0f, 0.0f), 30.0f, 0.1f, 1.5f,
+                             glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+
+    engineState.SetScene(scene);
+    engineState.SetCamera(camera);
+
+    Renderer::Init();
+
+    StartStartSystems();
+
+    physicsSystem.OptimizeBroadPhase();
+
+    MyContactListener* myListener = new MyContactListener();
+    physicsSystem.SetContactListener(myListener);
+
+    ImGuiLayer::Init();
+
+    MyDebugRenderer debugRenderer;
+
+    const JPH::BodyDrawFilter* filter = nullptr;
+
+    JPH::BodyManager::DrawSettings settings;
+    settings.mDrawShape = false;
+    settings.mDrawBoundingBox = true;
+    settings.mDrawShapeWireframe = true;
+
+    // render loop
+    // -----------
+    while (!window.ShouldClose())
+    {
+        ImGuiLayer::NewFrame();
+
+        UpdateSystems();
+
+        physTimer -= engineState.deltaTime;
+
+        physicsSystem.DrawBodies(settings, &debugRenderer, filter);
+
+        ImGuiLayer::EndFrame();
+        window.Update();
+
+        if (physTimer <= 0)
+        {
+            StepPhysics(engineState.deltaTime);
+        }
+    }
+
+    DestroyPhysics();
+    ImGuiLayer::Terminate();
+
+    return 0;
+}
